a2432371221b4c0e3b0de8dd17b3b52161eb911c
Jean Barcellos <jeanbarcellos@hotmail.com>
2019-07-16 21:35:40 -0300

    Finalizando exercícios de 62 a 65

---
+++ pythonexercicios/ex064.py
@@ -0,0 +1,10 @@
+num = 0
+cont = 0
+soma = 0
+
+while num != 999:
+    num = int(input('Digite um número [999 para parar] '))
+    if num != 999:
+        soma += num
+        cont += 1
+print('Você digitou {} números e a soma entre eles foi {}'.format(cont, soma))
