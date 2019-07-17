8636bf8ff79718a782f8b99c1a0f07202cfb77e2
Jean Barcellos <jean.barcellos@hevo.com.br>
2019-07-17 17:02:17 -0300

    Aula 16

---
+++ pythonteste/aula16.py
@@ -0,0 +1,11 @@
+# Aula 16 - Tuplas
+
+lanche = ('Hambúrger', 'Suco', 'Pizza', 'Pudim', 'Batata frita')
+
+for cont in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[cont]} na posicao {cont}')
+
+for comida in lanche:
+    print(f'Eu vou comer {comida}')
+
+print('Comi pra caramba!')
