f4ec4a39f10b3755a16495ad15e2a4d30510ca7b
Jean Barcellos <jeanbarcellos@hotmail.com>
2019-07-21 18:38:49 -0300

    Aula 19 + Ex 090 a 095

--- pythonexercicios/ex092.py
+++ pythonexercicios/ex092.py
@@ -1,1 +1,23 @@
 # Desafio 092
+# Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-o (com idade) em um dicionário.
+# Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário.
+# Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
+
+from datetime import datetime
+
+dados = dict()
+dados['nome'] = str(input(f'Nome: '))
+
+nasc = int(input(f'Ano de Nascimento: '))
+dados['idade'] = datetime.now().year - nasc
+
+dados['ctps'] = int(input('Carteira de Trabalho (0 não tem): '))
+
+if dados['ctps'] != 0:
+    dados['conratacao'] = int(input('Ano de Contraração: '))
+    dados['salario'] = float(input('Salário: R$ '))
+    dados['aposentadoris'] = dados['idade'] + ((dados['conratacao'] + 35) - datetime.now().year)
+
+print(dados)
+for k, v in dados.items():
+    print(f'  = {k} tem valor {v}')
--- pythonteste/aula19.py
+++ pythonteste/aula19.py
@@ -1,4 +1,31 @@
-pessoas = {'nome': 'Gustavo', 'sexo': 'M', 'idade': '22'}
+pessoas = {'nome': 'Gustavo', 'sexo': 'M', 'idade': 22}
 
 print(pessoas)
-print(f'O {pessoas["nome"]} tem {pessoas["idade"]} anos')
+print(f'O {pessoas["nome"]} tem {pessoas["idade"]} anos.')
+
+print(pessoas.keys())
+print(pessoas.values())
+print(pessoas.items())
+
+print('-' * 30)
+
+for k in pessoas.keys():
+    print(k)
+
+print('-' * 30)
+
+for k in pessoas.values():
+    print(k)
+
+print('-' * 30)
+
+for k, v in pessoas.items():
+    print(f'{k} = {v}')
+
+print('-' * 30)
+
+print(pessoas)
+del pessoas['sexo']
+print(pessoas)
+pessoas['nome'] = 'Leandro'
+print(pessoas)
